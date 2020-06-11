d6851fd76a3e8599f4e17c66db8baf95b1ba4dcd
nirholas <nirholas@users.noreply.github.com>
2020-06-11 14:06:56 -0700

    Add files via upload

---
+++ twitter_reply.py
@@ -0,0 +1,33 @@
+import csv
+import tweepy
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Oauth keys
+consumer_key = "XXX"
+consumer_secret = "XXX"
+access_token = "XXX"
+access_token_secret = "XXX"
+
+# Authentication with Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# update these for the tweet you want to process replies to 'name' = the account username and you can find the tweet id within the tweet URL
+name = 'LunarCRUSH'
+tweet_id = '1270923526690664448'
+
+replies=[]
+for tweet in tweepy.Cursor(api.search,q='to:'+name, result_type='recent', timeout=999999).items(1000):
+    if hasattr(tweet, 'in_reply_to_status_id_str'):
+        if (tweet.in_reply_to_status_id_str==tweet_id):
+            replies.append(tweet)
+
+with open('replies_clean.csv', 'w') as f:
+    csv_writer = csv.DictWriter(f, fieldnames=('user', 'text'))
+    csv_writer.writeheader()
+    for tweet in replies:
+        row = {'user': tweet.user.screen_name, 'text': tweet.text.replace('\n', ' ')}
+        csv_writer.writerow(row)
